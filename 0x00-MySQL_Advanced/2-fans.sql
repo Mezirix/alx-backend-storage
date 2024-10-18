21e6cd4380e8ca1a4aaea40962f481637c14af6a
Chimeziri Anyanwu <103989041+Mezirix@users.noreply.github.com>
2024-10-18 05:38:46 +0100

    Update 2-fans.sql

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,5 +1,5 @@
 -- a script that lists all bands with Glam rock as their main style, ranked by their longevity
-SELECT band_name, (IFNULL(split, '2022') - formed) AS lifespan
+SELECT origin, SUM(fans) AS nb_fans
     FROM metal_bands
-    WHERE FIND_IN_SET('Glam rock', IFNULL(style, "")) > 0
-    ORDER BY lifespan DESC;
+    GROUP BY origin
+    ORDER BY nb_fans DESC;
